89cd9ae9e1449a8b7fb8797ba591408e27c627f1
Archana <ms.archanacusat2015@gmail.com>
2023-03-29 19:10:29 +0530

    added return value demo from thread using callable and future interface

--- app/src/main/java/thread/executerocumbuilder/ExecuterServiceDriver.java
+++ app/src/main/java/thread/executerocumbuilder/ExecuterServiceDriver.java
@@ -1,7 +1,10 @@
 package thread.executerocumbuilder;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @Author: Archana Kumari
@@ -10,17 +13,55 @@ import java.util.concurrent.Executors;
 public class ExecuterServiceDriver {
     public static void main(String args[]) {
        // ExecutorService executorService = Executors.newFixedThreadPool(10); // more than one thread so parallel exeecution of task
-        ExecutorService executorService = Executors.newFixedThreadPool(1); // sequential  execution of task because only one thread in Thread pool
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        //ExecutorService executorService = Executors.newFixedThreadPool(1); // sequential  execution of task because only one thread in Thread pool
         /*executorService.execute(ExecuterDemoRunnable("Task1"));
         executorService.execute(ExecuterDemoRunnable("Task2"));
         executorService.execute(ExecuterDemoRunnable("Task3"));*/
         //or
-        executorService.execute(ExecuterDemoRunnable("Task3"));
+     /*   executorService.execute(ExecuterDemoRunnable("Task3"));
         executorService.execute(ExecuterDemoRunnable("Task1"));
         executorService.execute(ExecuterDemoRunnable("Task2"));
+        executorService.shutdown();*/
 
-        executorService.shutdown();
+      /*  // Get result and block main thread with runnable
+        Future future = executorService.submit(ExecuterDemoRunnable("Task1"));
+        System.out.println("Is it done before:" + future.isDone());
+        System.out.println("Cancel checked: before task is completed:" + future.cancel(true)); // true
+        try {
+            future.get();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
+        System.out.println("Is is done after:" + future.isDone());
+        System.out.println("Cancel checked: when task is completed:" + future.cancel(true)); // false*/
+
+        // Get result and block main thread with Callable
+        Future future = executorService.submit(ExecuterDemoCallable("Task1 using callable"));
+        try {
+            System.out.println("Returned value after computation using callable: " + future.get());
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static Callable<String> ExecuterDemoCallable(String taskName) {
+        return new Callable() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("I am in onCall");
+                return Thread.currentThread().getName() + ":" + taskName;
+            }
+        };
     }
 
     public static Runnable ExecuterDemoRunnable(String taskName) {
@@ -28,6 +69,7 @@ public class ExecuterServiceDriver {
             @Override
             public void run() {
                 System.out.println("Current Thread with task name:" + Thread.currentThread().getName() + ":" + taskName);
+                //wait(10000);
             }
         };
     }
---
+++ app/src/main/java/thread/executerocumbuilder/ExecuterServiceWithCallable.java
@@ -0,0 +1,8 @@
+package thread.executerocumbuilder;
+
+/**
+ * @Author: Archana Kumari
+ * @Date: 29-03-2023
+ */
+public class ExecuterServiceWithCallable {
+}
