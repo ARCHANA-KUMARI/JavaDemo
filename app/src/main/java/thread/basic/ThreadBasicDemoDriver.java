ceff6d25d1c49c7bc7b9ca1e2f53146adaec9783
ARCHANA KUMARI <ms.archanacusat2015@gmail.com>
2026-03-05 21:50:45 +0530

    Thread basic impl by extends Thread class

---
+++ app/src/main/java/thread/basic/ThreadBasicDemoDriver.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Shweta Kumari
+ * Date: 3/5/2026
+ */
+
+package thread.basic;
+
+public class ThreadBasicDemoDriver {
+    public static void main(String[] args) {
+        ThreadByExtend threadByExtend = new ThreadByExtend();
+        threadByExtend.start();
+    }
+}
---
+++ app/src/main/java/thread/basic/ThreadByExtend.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Shweta Kumari
+ * Date: 3/5/2026
+ */
+
+package thread.basic;
+
+public class ThreadByExtend extends  Thread{
+
+    public ThreadByExtend() {
+        System.out.println("ThreadByExtend constructor call");
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("ThreThreadByExtend run");
+    }
+}
