512a79e6eee275329f8dd75dd0b78373ff63d3a6
ARCHANA KUMARI <ms.archanacusat2015@gmail.com>
2026-03-06 22:37:55 +0530

    Diff between Thread`s run and start method

--- app/src/main/java/thread/basic/ThreadBasicDemoDriver.java
+++ app/src/main/java/thread/basic/ThreadBasicDemoDriver.java
@@ -7,10 +7,20 @@ package thread.basic;
 
 public class ThreadBasicDemoDriver {
     public static void main(String[] args) {
-//        ThreadByExtend threadByExtend = new ThreadByExtend();
-//        threadByExtend.start();
-        ThreadByImplRunnable threadByImplRunnable = new ThreadByImplRunnable();
+       ThreadByExtend threadByExtend = new ThreadByExtend();
+       threadByExtend.run();
+
+       //O/P using directly calling run method
+       // ThreadByExtend constructor call:Thread[#1,main,5,main], Thread`S Name:Thread-0
+       // ThreThreadByExtend run:Thread[#1,main,5,main], Thread`S Name:Thread-0
+
+      // threadByExtend.start();
+       // O/P
+       // ThreadByExtend constructor call:Thread[#1,main,5,main], Thread`S Name:Thread-0
+      //  ThreThreadByExtend run:Thread[#22,Thread-0,5,main], Thread`S Name:Thread-0
+
+        /*   ThreadByImplRunnable threadByImplRunnable = new ThreadByImplRunnable();
         Thread th = new Thread(threadByImplRunnable);
-        th.start();
+        th.start();*/
     }
 }
--- app/src/main/java/thread/basic/ThreadByExtend.java
+++ app/src/main/java/thread/basic/ThreadByExtend.java
@@ -8,12 +8,12 @@ package thread.basic;
 public class ThreadByExtend extends  Thread{
 
     public ThreadByExtend() {
-        System.out.println("ThreadByExtend constructor call");
+        System.out.println("ThreadByExtend constructor call:" + Thread.currentThread() + ", Thread`S Name:" + getName());
 
     }
 
     @Override
     public void run() {
-        System.out.println("ThreThreadByExtend run");
+        System.out.println("ThreThreadByExtend run:" + Thread.currentThread() + ", Thread`S Name:" + getName());
     }
 }
