289ad8704328b7425c73d75480637ad0e26635c7
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-04-26 22:03:58 +0530

    Final Methods Cannot Be Overridden

---
+++ app/src/main/java/Oops/opps/runtimemethodoverloading/RunTImeMethodOverloadingWIthFinalMethodChild.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Archana Kumari
+ * Date: 4/26/26
+ * File: RunTImeMethodOverloadingWIthFinalMethodChild.java
+ * Description: This class showing child class can`t override the final method.
+ */
+
+package Oops.opps.runtimemethodoverloading;
+
+public class RunTImeMethodOverloadingWIthFinalMethodChild extends RunTimeMethodOverloadingWIthFinalMethod {
+
+    //void show() {} // Compile time error : 'show()' cannot override 'show()' in 'Oops.opps.runtimemethodoverloading.RunTimeMethodOverloadingWIthFinalMethod'; overridden method is final
+}
---
+++ app/src/main/java/Oops/opps/runtimemethodoverloading/RunTimeMethodOverloadingWIthFinalMethod.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Archana Kumari
+ * Date: 4/26/26
+ * File: RunTimeMethodOverloadingWIthFinalMethod.java
+ * Description: This class shows that final method can`t be override my it`s child class.
+ *
+ */
+
+package Oops.opps.runtimemethodoverloading;
+
+public class RunTimeMethodOverloadingWIthFinalMethod {
+    final void show(){
+        System.out.println("RunTimeMethodOverloadingWIthFinalMethod.show");
+    }
+}
